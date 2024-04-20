21629da9b6cc3da2a9bdd24f31b6838a4f47c05a
chaithrasaig <chaitrasai12@gmail.com>
2024-04-20 14:17:30 +0000

    Revert "Revert "shell files committed""
    This reverts commit 0f99b51c00754235040e402a6821422954b5bf15.

---
+++ arthemetic.sh
@@ -0,0 +1,6 @@
+echo " please enter the Devops tools.. "
+read -a devops tools
+echo " the Devops tools  which you have typed is:"${Devops tools[*]}
+echo " print the 4Th value : "{ Devops tools [3] }
+
+
---
+++ userdef.sh
@@ -0,0 +1,9 @@
+a=10;
+b=20;
+c=30;
+name='chaitra'
+echo $a
+echo $b
+echo $c
+echo $name
+
